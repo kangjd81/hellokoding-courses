0a7435b6e939e5b471ea2087c6f4d482f3a55d76
강지돈 <kangjd81@gmail.com>
2019-05-02 10:26:27 +0900

    Update IndexController.java

--- docker-examples/dockercompose-springboot-nginx/app/src/main/java/com/hellokoding/springboot/IndexController.java
+++ docker-examples/dockercompose-springboot-nginx/app/src/main/java/com/hellokoding/springboot/IndexController.java
@@ -1,14 +1,20 @@
 package com.hellokoding.springboot;
 
+import java.util.Random;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class IndexController {
-    @GetMapping("/")
-    public String index() {
+	Random ran = new Random();
+	String appId = String.valueOf(ran.nextInt(1000));
+
+	@GetMapping("/")
+    public String index(Model model) {
+    	
+		model.addAttribute("appId", appId);
         return "index";
     }
 }
